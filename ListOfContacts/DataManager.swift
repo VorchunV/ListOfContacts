ea82d0f26fc1fc8704bf4bfc4623a89803cb5b1c
VorchunV <varenkoluchka@gmail.com>
2021-11-10 12:17:45 +0300

    DataManager class

---
+++ ListOfContacts/DataManager.swift
@@ -0,0 +1,21 @@
+//
+//  DataManager.swift
+//  ListOfContacts
+//
+//  Created by Варвара Фомина on 10.11.2021.
+//
+
+
+class DataManager {
+    
+    let names = ["Arthur", "Brandon", "Zachary", "Calvin", "Branson", "Hugh"]
+    
+    let surnames = ["Cox", "Edwards", "Rogers", "Flores", "Miller", "Griffin"]
+    
+    let emails = [
+        "erer@mail.ru", "dfdf@mail.ru", "asas@mail.ru", "cvcv@mail.ru",
+        "nmnm@mail.ru", "jkjk@mail.ru"
+    ]
+    
+    let phoneNumbers = [ "34344", "45455", "56566", "67677", "78788", "89899"]
+}
